95055f3e6f2e05a7e2f7b60ff9b8b4b7cb2e811d
sttrackstar <sonalt321@gmail.com>
2026-07-12 11:36:23 -0400

    Assignment completed

--- server/app.py
+++ server/app.py
@@ -1,10 +1,30 @@
-#!/usr/bin/env python3
+from flask import Flask, make_response
 
-from flask import Flask, request, current_app, g, make_response
+contracts = [
+    {"id": 1, "contract_information": "This contract is for John and building a shed"},
+    {"id": 2, "contract_information": "This contract is for a deck for a buisiness"},
+    {"id": 3, "contract_information": "This contract is to confirm ownership of this car"}
+]
 
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
-customers = ["bob","bill","john","sarah"]
+customers = ["bob", "bill", "john", "sarah"]
+
 app = Flask(__name__)
 
-if __name__ == '__main__':
+
+@app.route("/contract/<int:id>")
+def contract_info(id):
+    for contract in contracts:
+        if contract["id"] == id:
+            return make_response(contract["contract_information"], 200)
+    return make_response("", 404)
+
+
+@app.route("/customer/<customer_name>")
+def customer_info(customer_name):
+    if customer_name in customers:
+        return make_response("", 204)
+    return make_response("", 404)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
